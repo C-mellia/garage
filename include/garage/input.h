85cc13ffe7f3b5ec938886aa1cf0464f191938bd
C-mellia <m13418875018@gmail.com>
2024-09-30 14:05:06 +0800

    new file:   include/garage/input.h 	new file:   src/input.c

---
+++ include/garage/input.h
@@ -0,0 +1,29 @@
+#ifndef GARAGE_INPUT_H
+#define GARAGE_INPUT_H 1
+
+#include <garage/garage.h>
+
+// A text file format to seperate different test cases and expected output
+// respectively
+
+typedef struct Test {
+    Phantom input;
+
+    void *_input_mem;
+    size_t _input_align, _input_len;
+
+    Phantom res;
+
+    void *_res_mem;
+    size_t _res_align, _res_len;
+} *Test;
+
+void test_init(Test test, size_t align);
+Test test_new(size_t align);
+void test_cleanup(Test test);
+void test_drop(Test *test);
+
+int test_deb_dprint(int fd, Test test);
+int test_deb_print(Test test);
+
+#endif // GARAGE_INPUT_H
---
+++ src/input.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+#include <garage/input.h>
+#include <garage/string.h>
+#include <garage/slice.h>
+#include <garage/log.h>
+
+static inline void __test_init(Test test, size_t align);
+
+void test_init(Test test, size_t align) {
+    nul_check(Test, test), __test_init(test, align);
+}
+
+Test test_new(size_t align) {
+    Test test = malloc(sizeof *test);
+    alloc_check(malloc, test, sizeof *test);
+    return __test_init(test, align), test;
+}
+
+void test_cleanup(Test test) {
+    if (!test) return;
+    Slice input = (void *)test->input, res = (void *)test->res;
+    slice_cleanup(input), slice_cleanup(res);
+}
+
+void test_drop(Test *test) {
+    if (test && *test) test_cleanup(*test), free(*test), *test = 0;
+}
+
+int test_deb_dprint(int fd, Test test) {
+    if (!test) return dprintf(fd, "(nil)");
+    Slice input = (void *)test->input, res = (void *)test->res;
+    String Cleanup(string_drop) string = string_new();
+    string_fmt(string, "{input: ");
+    string_fmt_func(string, (void *)slice_deb_dprint, input);
+    string_fmt(string, ", res: ");
+    string_fmt_func(string, (void *)slice_deb_dprint, res);
+    string_fmt(string, "}");
+    return string_dprint(fd, string);
+}
+
+int test_deb_print(Test test) {
+    return test_deb_dprint(1, test);
+}
+
+static void __test_init(Test test, size_t align) {
+    Slice input = (void *)test->input, res = (void *)test->res;
+    slice_init(input, 0, align, 0), slice_init(res, 0, align, 0);
+}
