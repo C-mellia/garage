dbda3e4155f86f4bb294b0971c9796b949cd1a46
C-mellia <m13418875018@gmail.com>
2024-10-07 14:05:04 +0800

    new file:   include/garage/prelude.h

---
+++ include/garage/prelude.h
@@ -0,0 +1,10 @@
+#ifndef GARAGE_PRELUDE_H
+#define GARAGE_PRELUDE_H 1
+
+#include <garage/garage.h>
+#include <garage/log.h>
+#include <garage/string.h>
+#include <garage/array.h>
+#include <garage/slice.h>
+
+#endif // GARAGE_PRELUDE_H
